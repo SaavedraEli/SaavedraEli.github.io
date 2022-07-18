56c7415858f83de3e2c3385d7e14bab923b7eae5
“Elizabeth <eli.avicolli@gmail.com>
2022-07-17 23:04:18 -0300

    final

--- TPINTEGRADOR/script-alta.js
+++ TPINTEGRADOR/script-alta.js
@@ -6,6 +6,12 @@ form.addEventListener("submit", function(event) {
     formulario.get("formNombre");
     formulario.get("formPrecio");
     formulario.get("formCodigo");
+    insertRowinTable(formulario);
+    
+}   
+
+)
+function insertRowinTable(formulario){
     let tableRef = document.getElementById("table");
     let newtableRowRef = tableRef.insertRow(-1);
     let newtableCellRef = newtableRowRef.insertCell(0);
@@ -14,7 +20,4 @@ form.addEventListener("submit", function(event) {
     newtableCellRef.textContent = formulario.get("formPrecio");
     let newtableCellRef = newtableRowRef.insertCell(2);
     newtableCellRef.textContent = formulario.get("formCodigo");
-    
-}   
-
-)
+}
